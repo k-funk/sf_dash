d23c71a2131011fe49b49e23cfbc03185f4b7dac
Kevin Funk <kevincfunk@gmail.com>
2015-07-09 17:39:32 -0700

    Yeoman+Angular starting point

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sfDashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sfDashApp
+ */
+angular.module('sfDashApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
