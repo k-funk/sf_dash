cd9c20cd7352579776f92b74674b5796913bd581
Kevin Funk <kevincfunk@gmail.com>
2019-07-22 13:56:33 -0300

    cleanup a bit more

--- src/scripts/services/nextBusSvc.js
+++ src/scripts/services/nextBusSvc.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import X2JS from 'x2js';
 
 
+const URL = 'http://webservices.nextbus.com/service/publicXMLFeed';
 const ensureArray = o => (Array.isArray(o) ? o : [o]);
 
 const sortPredictions = (stopRouteTags, predictions) => {
@@ -13,6 +14,24 @@ const sortPredictions = (stopRouteTags, predictions) => {
   return sortedList;
 };
 
+const mergeRouteData = routeData => {
+  routeData.forEach(route => {
+    // map the stop tags
+    const stopTags = {};
+    route.stop.forEach(stop => {
+      stopTags[stop._tag] = stop;
+    });
+
+    route.direction.forEach(direction => {
+      for (let i = 0; i < direction.stop.length; i++) {
+        const tag = direction.stop[i]._tag;
+        direction.stop[i] = stopTags[tag];
+      }
+    });
+  });
+  return routeData;
+};
+
 /**
  * @ngdoc service
  * @name sfDashApp.nextBusSvc
@@ -21,91 +40,70 @@ const sortPredictions = (stopRouteTags, predictions) => {
  * Factory in the sfDashApp.
  */
 angular.module('sfDashApp')
-  .factory('nextBusSvc', ($http, $q, geolocation) => {
-    const url = 'http://webservices.nextbus.com/service/publicXMLFeed';
-    const mergeRouteData = routeData => {
-      routeData.forEach(route => {
-        // map the stop tags
-        const stopTags = {};
-        route.stop.forEach(stop => {
-          stopTags[stop._tag] = stop;
+  .factory('nextBusSvc', ($http, $q, geolocation) => ({
+    getPredictions(stopRouteTags) {
+      return $http.get(URL, {
+        params: {
+          command: 'predictionsForMultiStops',
+          a: 'sf-muni',
+          stops: stopRouteTags,
+          useShortTitles: true,
+        },
+      })
+        .then(data => {
+          const jsonObj = new X2JS().xml2js(data.data);
+          const predictions = ensureArray(jsonObj.body.predictions);
+          return sortPredictions(stopRouteTags, predictions);
         });
+    },
+    getRouteConfig() {
+      return $http.get(URL, {
+        cache: true,
+        params: {
+          command: 'routeConfig',
+          a: 'sf-muni',
+          useShortTitles: true,
+        },
+      })
+        .then(data => mergeRouteData(data.data.body.route));
+    },
+    getStopsWithin(meters) {
+      return $q.all([
+        geolocation.getLocation(),
+        this.getRouteConfig(),
+      ]).then(promises => {
+        /** Once we've got both the user's location and the route Data search */
+        const stopResults = [];
+        const currPosition = new google.maps.LatLng(
+          promises[0].coords.latitude,
+          promises[0].coords.longitude,
+        );
+        const routeConfig = promises[1];
 
-        route.direction.forEach(direction => {
-          for (let i = 0; i < direction.stop.length; i++) {
-            const tag = direction.stop[i]._tag;
-            direction.stop[i] = stopTags[tag];
-          }
-        });
-      });
-      return routeData;
-    };
-
-    return {
-      getPredictions(stopRouteTags) {
-        return $http.get(url, {
-          params: {
-            command: 'predictionsForMultiStops',
-            a: 'sf-muni',
-            stops: stopRouteTags,
-            useShortTitles: true,
-          },
-        })
-          .then(data => {
-            const jsonObj = new X2JS().xml2js(data.data);
-            const predictions = ensureArray(jsonObj.body.predictions);
-            return sortPredictions(stopRouteTags, predictions);
-          });
-      },
-      getRouteConfig() {
-        return $http.get(url, {
-          cache: true,
-          params: {
-            command: 'routeConfig',
-            a: 'sf-muni',
-            useShortTitles: true,
-          },
-        })
-          .then(data => mergeRouteData(data.data.body.route));
-      },
-      getStopsWithin(meters) {
-        return $q.all([
-          geolocation.getLocation(),
-          this.getRouteConfig(),
-        ]).then(promises => {
-          /** Once we've got both the user's location and the route Data search */
-          const stopResults = [];
-          const currPosition = new google.maps.LatLng(
-            promises[0].coords.latitude,
-            promises[0].coords.longitude,
-          );
-          const routeConfig = promises[1];
-
-          routeConfig.forEach(route => {
-            route.direction.forEach(direction => {
-              direction.stop.forEach(stop => {
-                /** Find stops that are within [meters] of the users position */
-                const stopLatLng = new google.maps.LatLng(stop._lat, stop._lon);
-                const distance = google.maps.geometry.spherical
-                  .computeDistanceBetween(currPosition, stopLatLng);
-                if (distance <= meters) {
-                  stopResults.push({
-                    tag: route._tag,
-                    title: route._title,
-                    directionName: direction._name,
-                    directionTitle: direction._title,
-                    latLng: stopLatLng,
-                    stopId: stop._stopId,
-                    stopTag: stop._tag,
-                    stopTitle: stop._title,
-                  });
-                }
-              });
+        routeConfig.forEach(route => {
+          route.direction.forEach(direction => {
+            direction.stop.forEach(stop => {
+              /** Find stops that are within [meters] of the users position */
+              const stopLatLng = new google.maps.LatLng(stop._lat, stop._lon);
+              const distance = google.maps.geometry.spherical
+                .computeDistanceBetween(currPosition, stopLatLng);
+              if (distance <= meters) {
+                stopResults.push({
+                  tag: route._tag,
+                  title: route._title,
+                  directionName: direction._name,
+                  directionTitle: direction._title,
+                  latLng: stopLatLng,
+                  stopId: stop._stopId,
+                  stopTag: stop._tag,
+                  stopTitle: stop._title,
+                });
+              }
             });
           });
-
-          return stopResults;
         });
-      },
-    };
-  });
+
+        return stopResults;
+      });
+    },
+  }));
--- src/scripts/services/weatherSvc.js
+++ src/scripts/services/weatherSvc.js
@@ -22,48 +22,48 @@ import angular from 'angular';
 //   'ozone': 303.4,
 // }
 
-// Example D
-// //   'time':1563692400,
-// //   'summary':'Partly cloudy throughout the day.',
-// //   'icon':'partly-cloudy-day',
-// //   'sunriseTime':1563714332,
-// //   'sunsetTime':1563766133,
-// //   'moonPhase':0.66,
-// //   'precipIntensity':0.0001,
-// //   'precipIntensityMax':0.0017,
-// //   'precipIntensityMaxTime':1563771600,
-// //   'precipProbability':0.09,
-// //   'precipType':'rain',
-// //   'temperatureHigh':65.95,
-// //   'temperatureHighTime':1563757200,
-// //   'temperatureLow':56.69,
-// //   'temperatureLowTime':1563800400,
-// //   'apparentTemperatureHigh':65.95,
-// //   'apparentTemperatureHighTime':1563757200,
-// //   'apparentTemperatureLow':56.69,
-// //   'apparentTemperatureLowTime':1563800400,
-// //   'dewPoint':54.13,
-// //   'humidity':0.81,
-// //   'pressure':1013.6,
-// //   'windSpeed':6.05,
-// //   'windGust':12.09,
-// //   'windGustTime':1563742800,
-// //   'windBearing':258,
-// //   'cloudCover':0.47,
-// //   'uvIndex':10,
-// //   'uvIndexTime':1563739200,
-// //   'visibility':8.265,
-// //   'ozone':305.1,
-// //   'temperatureMin':55.82,
-// //   'temperatureMinTime':1563721200,
-// //   'temperatureMax':65.95,
-// //   'temperatureMaxTime':1563757200,
-// //   'apparentTemperatureMin':55.82,
-// //   'apparentTemperatureMinTime':1563721200,
-// //   'apparentTemperatureMax':65.95,
-// //   'apparentTemperatureMaxTime':1563757200
-// // }arkSky Daily Data
+// Example DarkSky Daily Data
 // {
+//   'time':1563692400,
+//   'summary':'Partly cloudy throughout the day.',
+//   'icon':'partly-cloudy-day',
+//   'sunriseTime':1563714332,
+//   'sunsetTime':1563766133,
+//   'moonPhase':0.66,
+//   'precipIntensity':0.0001,
+//   'precipIntensityMax':0.0017,
+//   'precipIntensityMaxTime':1563771600,
+//   'precipProbability':0.09,
+//   'precipType':'rain',
+//   'temperatureHigh':65.95,
+//   'temperatureHighTime':1563757200,
+//   'temperatureLow':56.69,
+//   'temperatureLowTime':1563800400,
+//   'apparentTemperatureHigh':65.95,
+//   'apparentTemperatureHighTime':1563757200,
+//   'apparentTemperatureLow':56.69,
+//   'apparentTemperatureLowTime':1563800400,
+//   'dewPoint':54.13,
+//   'humidity':0.81,
+//   'pressure':1013.6,
+//   'windSpeed':6.05,
+//   'windGust':12.09,
+//   'windGustTime':1563742800,
+//   'windBearing':258,
+//   'cloudCover':0.47,
+//   'uvIndex':10,
+//   'uvIndexTime':1563739200,
+//   'visibility':8.265,
+//   'ozone':305.1,
+//   'temperatureMin':55.82,
+//   'temperatureMinTime':1563721200,
+//   'temperatureMax':65.95,
+//   'temperatureMaxTime':1563757200,
+//   'apparentTemperatureMin':55.82,
+//   'apparentTemperatureMinTime':1563721200,
+//   'apparentTemperatureMax':65.95,
+//   'apparentTemperatureMaxTime':1563757200
+// }
 
 
 // all params required
@@ -85,6 +85,7 @@ const getUrl = ({
  */
 angular.module('sfDashApp')
   .factory('weatherSvc', ($http, $q, $localStorage, $sce) => {
+    // $http.jsonp and $sce is needed to get around CORS
     const fetchWeatherData = ({ lat, long, key }) => $http.jsonp(
       $sce.trustAsResourceUrl(getUrl({ lat, long, key })),
     ).then(results => {
